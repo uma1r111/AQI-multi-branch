3ada21a4424aa0cc2641206d211d03535c663865
Shaikh Muhammad Umair <shaikh.mumar@systemsltd.com>
2025-08-04 12:32:15 +0500

    updated compare_model.py

--- model training/compare_model.py
+++ model training/compare_model.py
@@ -10,7 +10,7 @@ from mlflow.exceptions import MlflowException
 # -------------------------------
 MLFLOW_TRACKING_URI = "http://172.174.154.85:8000"
 EXPERIMENT_NAME = "Pre-Prod Model Training"
-METRICS_PATH = r"C:\Users\shaikh.mumar\AQI-multi-branch\metrics.json"
+METRICS_PATH = "metrics.json"
 CHALLENGER_METRICS_PATH = "challenger-posttest_metrics.json"
 MODEL_NAME = "aqi-model"
 ALIAS_CHALLENGER = "challenger-pretest"
